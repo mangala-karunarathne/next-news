0c2b2d2f3820e42d1dc07eebe0c48cf34a37c814
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-09 08:54:21 +0530

    CHG | Initial Commit - Project Setup

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+const About = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+const Index = () => {
+    return (
+        <div>
+           <h2>Hello from next JS</h2>
+          <Link href="/about">About</Link>
+        </div>
+      );
+};
+
+export default Index;
